9b51edbaf0d8b0fbd7cb67036bfa8140f75c0633
Florian Luther <Florian.Luther@akelius.de>
2021-10-22 15:45:51 +0200

    feat: add CRUD operations for stored procedures

--- src/cosmos-db/stored-procedures.ts
+++ src/cosmos-db/stored-procedures.ts
@@ -1,30 +1,62 @@
-import { CosmosClient, StoredProcedureDefinition } from '@azure/cosmos'
+import { Container, CosmosClient, StoredProcedureDefinition, StoredProcedureResponse } from '@azure/cosmos'
 import { AppSettings } from '../configuration'
 
 export async function getNames(databaseName: string, containerName: string): Promise<string[]> {
-    const settings = AppSettings.instance
-    const client: CosmosClient = new CosmosClient(settings.cosmosDbConnectionString)
-
-    const database = client.database(databaseName)
-    const container = database.container(containerName)
-    const result = await container.scripts.storedProcedures.readAll().fetchAll()
-
-    return result.resources.map((sp) => sp.id)
+    const container = getCosmosDbContainer(databaseName, containerName)
+    return await fetchNames(container)
 }
 
 export async function create(databaseName: string, containerName: string, id: string, body: string): Promise<number> {
-    const settings = AppSettings.instance
-    const client: CosmosClient = new CosmosClient(settings.cosmosDbConnectionString)
-
-    const database = client.database(databaseName)
-    const container = database.container(containerName)
-
+    const container = getCosmosDbContainer(databaseName, containerName)
     const definition: StoredProcedureDefinition = {
         id,
         body,
     }
 
-    const result = await container.scripts.storedProcedures.create(definition)
+    const isExisting = await exists(container, id)
+
+    let result: StoredProcedureResponse
+    if (!isExisting) {
+        // 201 Created
+        result = await container.scripts.storedProcedures.create(definition)
+    } else {
+        // 200 OK
+        result = await container.scripts.storedProcedure(id).replace(definition)
+    }
 
     return result.statusCode
+}
+
+export async function remove(databaseName: string, containerName: string, id: string): Promise<number> {
+    const container = getCosmosDbContainer(databaseName, containerName)
+
+    const isExisting = await exists(container, id)
+    if (!isExisting) {
+        return 404
+    }
+
+    // 204 No Content
+    const result = await container.scripts.storedProcedure(id).delete()
+
+    return result.statusCode
+}
+
+async function fetchNames(container: Container): Promise<string[]> {
+    const result = await container.scripts.storedProcedures.readAll().fetchAll()
+    return result.resources.map((sp) => sp.id)
+}
+
+async function exists(container: Container, id: string): Promise<boolean> {
+    const names = await fetchNames(container)
+    return names.includes(id)
+}
+
+function getCosmosDbContainer(databaseName: string, containerName: string): Container {
+    const settings = AppSettings.instance
+    const client: CosmosClient = new CosmosClient(settings.cosmosDbConnectionString)
+
+    const database = client.database(databaseName)
+    const container = database.container(containerName)
+
+    return container
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { ConsoleLogger } from '@florianluther/ftl-logger'
-import { AppSettings } from './configuration'
-import { create, getNames } from './cosmos-db/stored-procedures'
+import { ConsoleLogger, ILogger } from '@florianluther/ftl-logger'
+import { AppSettings, IConfiguration } from './configuration'
+import { create, getNames, remove as del } from './cosmos-db/stored-procedures'
+
+const StoredProcedureId = 'my-test-stored-procedure'
 
 export async function run(): Promise<void> {
     const settings = AppSettings.instance
     const logger = new ConsoleLogger()
 
+    logger.log('##### READ #####', 'green')
+    await read(settings, logger)
+
+    logger.log('##### WRITE #####', 'orange')
+    await write(settings, logger)
+
+    logger.log('##### DELETE #####', 'red')
+    await remove(settings, logger)
+}
+
+async function read(settings: IConfiguration, logger: ILogger): Promise<void> {
     logger.log(
         `Loading all stored procedures of the database '${settings.database}' and the container '${settings.container}'...`
     )
@@ -19,14 +32,20 @@ export async function run(): Promise<void> {
     } else {
         logger.log(names)
     }
+}
 
-    const result = await create(
-        settings.database,
-        settings.container,
-        'test-stored-procedure',
-        `function testStoredProcedure() { console.log('test stored procedure') }`
-    )
-    logger.log(`Result: ${result}`)
+async function write(settings: IConfiguration, logger: ILogger): Promise<void> {
+    const body = `function myTestStoredProcedure() { console.log('${StoredProcedureId}') }`
+    logger.log(`Creating or updating stored procedure '${StoredProcedureId}'...`)
+
+    const result = await create(settings.database, settings.container, StoredProcedureId, body)
+    logger.log(`Status code result: ${result}`)
+}
+
+async function remove(settings: IConfiguration, logger: ConsoleLogger) {
+    logger.log(`Deleting the stored procedure '${StoredProcedureId}'...`)
+    const result = await del(settings.database, settings.container, StoredProcedureId)
+    logger.log(`Status code result: ${result}`)
 }
 
 run()
